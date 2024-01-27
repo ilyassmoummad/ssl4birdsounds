817b10f70c351b2a45209f70c6931eadf477b186
Ilyass Moummad <mr.ilyassmoummad@gmail.com>
2024-01-27 01:07:45 +0100

    Update panns.py

--- util/panns.py
+++ util/panns.py
@@ -1,83 +1,72 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchlibrosa.stft import Spectrogram, LogmelFilterBank
-from torchlibrosa.augmentation import SpecAugmentation
-
-
-def do_mixup(x, mixup_lambda):
-    """Mixup x of even indexes (0, 2, 4, ...) with x of odd indexes 
-    (1, 3, 5, ...).
-
-    Args:
-      x: (batch_size * 2, ...)
-      mixup_lambda: (batch_size * 2,)
-
-    Returns:
-      out: (batch_size, ...)
-    """
-    out = (x[0 :: 2].transpose(0, -1) * mixup_lambda[0 :: 2] + \
-        x[1 :: 2].transpose(0, -1) * mixup_lambda[1 :: 2]).transpose(0, -1)
-    return out
-
-def interpolate(x, ratio):
-    """Interpolate data in time domain. This is used to compensate the 
-    resolution reduction in downsampling of a CNN.
-    
-    Args:
-      x: (batch_size, time_steps, classes_num)
-      ratio: int, ratio to interpolate
-
-    Returns:
-      upsampled: (batch_size, time_steps * ratio, classes_num)
-    """
-    (batch_size, time_steps, classes_num) = x.shape
-    upsampled = x[:, :, None, :].repeat(1, 1, ratio, 1)
-    upsampled = upsampled.reshape(batch_size, time_steps * ratio, classes_num)
-    return upsampled
-
-def pad_framewise_output(framewise_output, frames_num):
-    """Pad framewise_output to the same length as input frames. The pad value 
-    is the same as the value of the last frame.
-
-    Args:
-      framewise_output: (batch_size, frames_num, classes_num)
-      frames_num: int, number of frames to pad
-
-    Outputs:
-      output: (batch_size, frames_num, classes_num)
-    """
-    pad = framewise_output[:, -1 :, :].repeat(1, frames_num - framewise_output.shape[1], 1)
-    """tensor for padding"""
-
-    output = torch.cat((framewise_output, pad), dim=1)
-    """(batch_size, frames_num, classes_num)"""
-
-    return output
 
 def init_layer(layer):
     """Initialize a Linear or Convolutional layer. """
     nn.init.xavier_uniform_(layer.weight)
- 
     if hasattr(layer, 'bias'):
         if layer.bias is not None:
             layer.bias.data.fill_(0.)
             
-    
 def init_bn(bn):
     """Initialize a Batchnorm layer. """
     bn.bias.data.fill_(0.)
     bn.weight.data.fill_(1.)
 
-
-class ConvBlock5x5(nn.Module):
+class ConvBlock(nn.Module): #for CNN10 & CNN14
     def __init__(self, in_channels, out_channels):
+        
+        super(ConvBlock, self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels=in_channels, 
+                              out_channels=out_channels,
+                              kernel_size=(3, 3), stride=(1, 1),
+                              padding=(1, 1), bias=False)
+                              
+        self.conv2 = nn.Conv2d(in_channels=out_channels, 
+                              out_channels=out_channels,
+                              kernel_size=(3, 3), stride=(1, 1),
+                              padding=(1, 1), bias=False)
+                              
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.init_weight()
+        
+    def init_weight(self):
+        init_layer(self.conv1)
+        init_layer(self.conv2)
+        init_bn(self.bn1)
+        init_bn(self.bn2)
+
+        
+    def forward(self, input, pool_size=(2, 2), pool_type='avg'):
+        
+        x = input
+        x = F.relu_(self.bn1(self.conv1(x)))
+        x = F.relu_(self.bn2(self.conv2(x)))
+        if pool_type == 'max':
+            x = F.max_pool2d(x, kernel_size=pool_size)
+        elif pool_type == 'avg':
+            x = F.avg_pool2d(x, kernel_size=pool_size)
+        elif pool_type == 'avg+max':
+            x1 = F.avg_pool2d(x, kernel_size=pool_size)
+            x2 = F.max_pool2d(x, kernel_size=pool_size)
+            x = x1 + x2
+        else:
+            raise Exception('Incorrect argument!')
+        
+        return x
+
+class ConvBlock5x5(nn.Module): #for CNN6
+    def __init__(self, in_channels, out_channels, stride=(1,1)):
         
         super(ConvBlock5x5, self).__init__()
         
         self.conv1 = nn.Conv2d(in_channels=in_channels, 
                               out_channels=out_channels,
-                              kernel_size=(5, 5), stride=(1, 1),
+                              kernel_size=(5, 5), stride=stride,
                               padding=(2, 2), bias=False)
                               
         self.bn1 = nn.BatchNorm2d(out_channels)
@@ -106,87 +95,289 @@ class ConvBlock5x5(nn.Module):
         
         return x
     
-
-class Cnn6(nn.Module):
-    def __init__(self, sample_rate, window_size, hop_size, mel_bins, fmin, 
-        fmax, classes_num):
+class ConvBlock5(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=(1,1)):
         
-        super(Cnn6, self).__init__()
-
-        window = 'hann'
-        center = True
-        pad_mode = 'reflect'
-        ref = 1.0
-        amin = 1e-10
-        top_db = None
-
-        # Spectrogram extractor
-        self.spectrogram_extractor = Spectrogram(n_fft=window_size, hop_length=hop_size, 
-            win_length=window_size, window=window, center=center, pad_mode=pad_mode, 
-            freeze_parameters=True)
-
-        # Logmel feature extractor
-        self.logmel_extractor = LogmelFilterBank(sr=sample_rate, n_fft=window_size, 
-            n_mels=mel_bins, fmin=fmin, fmax=fmax, ref=ref, amin=amin, top_db=top_db, 
-            freeze_parameters=True)
-
-        # Spec augmenter
-        self.spec_augmenter = SpecAugmentation(time_drop_width=64, time_stripes_num=2, 
-            freq_drop_width=8, freq_stripes_num=2)
-
-        self.bn0 = nn.BatchNorm2d(64)
-
-        self.conv_block1 = ConvBlock5x5(in_channels=1, out_channels=64)
-        self.conv_block2 = ConvBlock5x5(in_channels=64, out_channels=128)
-        self.conv_block3 = ConvBlock5x5(in_channels=128, out_channels=256)
-        self.conv_block4 = ConvBlock5x5(in_channels=256, out_channels=512)
-
-        self.fc1 = nn.Linear(512, 512, bias=True)
-        self.fc_audioset = nn.Linear(512, classes_num, bias=True)
+        super(ConvBlock5, self).__init__()
         
+        self.conv1 = nn.Conv2d(in_channels=in_channels, 
+                              out_channels=out_channels,
+                              kernel_size=(3, 5), stride=stride,
+                              padding=(2, 2), bias=False)
+        
+        self.conv2 = nn.Conv2d(in_channels=out_channels, 
+                              out_channels=out_channels,
+                              kernel_size=(3, 5), stride=stride,
+                              padding=(2, 2), bias=False)
+                              
+        self.bn1 = nn.BatchNorm2d(out_channels)
+                              
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
         self.init_weight()
-
+        
     def init_weight(self):
-        init_bn(self.bn0)
-        init_layer(self.fc1)
-        init_layer(self.fc_audioset)
- 
-    def forward(self, input, mixup_lambda=None):
-        """
-        Input: (batch_size, data_length)"""
+        init_layer(self.conv1)
+        init_layer(self.conv2)
+        init_bn(self.bn1)
+        init_bn(self.bn2)
 
-        x = self.spectrogram_extractor(input)   # (batch_size, 1, time_steps, freq_bins)
-        x = self.logmel_extractor(x)    # (batch_size, 1, time_steps, mel_bins)
+    def forward(self, input, pool_size=(2, 2), pool_type='avg'):
         
-        x = x.transpose(1, 3)
-        x = self.bn0(x)
-        x = x.transpose(1, 3)
+        x = input
+        x = F.relu_(self.bn1(self.conv1(x)))
+        x = F.relu_(self.bn2(self.conv2(x)))
+        if pool_type == 'max':
+            x = F.max_pool2d(x, kernel_size=pool_size)
+        elif pool_type == 'avg':
+            x = F.avg_pool2d(x, kernel_size=pool_size)
+        elif pool_type == 'avg+max':
+            x1 = F.avg_pool2d(x, kernel_size=pool_size)
+            x2 = F.max_pool2d(x, kernel_size=pool_size)
+            x = x1 + x2
+        else:
+            raise Exception('Incorrect argument!')
         
-        if self.training:
-            x = self.spec_augmenter(x)
+        return x
+    
+class CNN10x5(nn.Module):
+    def __init__(self, num_classes=4, do_dropout=False, embed_only=False):
+        super(CNN10x5, self).__init__()
 
-        # Mixup on spectrogram
-        if self.training and mixup_lambda is not None:
-            x = do_mixup(x, mixup_lambda)
-        
+        self.embed_only = embed_only
+        self.do_dropout = do_dropout
+        self.conv_block1 = ConvBlock5(in_channels=1, out_channels=64)
+        self.conv_block2 = ConvBlock5(in_channels=64, out_channels=128)
+        self.conv_block3 = ConvBlock5(in_channels=128, out_channels=256)
+        self.conv_block4 = ConvBlock5(in_channels=256, out_channels=512)
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x):
+
         x = self.conv_block1(x, pool_size=(2, 2), pool_type='avg')
-        x = F.dropout(x, p=0.2, training=self.training)
+        if self.do_dropout:
+            x = self.dropout(x)
         x = self.conv_block2(x, pool_size=(2, 2), pool_type='avg')
-        x = F.dropout(x, p=0.2, training=self.training)
+        if self.do_dropout:
+            x = self.dropout(x)
         x = self.conv_block3(x, pool_size=(2, 2), pool_type='avg')
-        x = F.dropout(x, p=0.2, training=self.training)
+        if self.do_dropout:
+            x = self.dropout(x)
         x = self.conv_block4(x, pool_size=(2, 2), pool_type='avg')
-        x = F.dropout(x, p=0.2, training=self.training)
-        x = torch.mean(x, dim=3)
-        
-        (x1, _) = torch.max(x, dim=2)
-        x2 = torch.mean(x, dim=2)
-        x = x1 + x2
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = F.relu_(self.fc1(x))
-        embedding = F.dropout(x, p=0.5, training=self.training)
-        clipwise_output = torch.sigmoid(self.fc_audioset(x))
-        
-        output_dict = {'clipwise_output': clipwise_output, 'embedding': embedding}
+        if self.do_dropout:
+            x = self.dropout(x)
 
-        return output_dict
+        x = torch.mean(x, dim=3) #mean over time dim
+        (x1, _) = torch.max(x, dim=2) #max over freq dim
+        x2 = torch.mean(x, dim=2) #mean over freq dim (after mean over time)
+        x = x1 + x2
+
+        return x
+
+class _CNN6(nn.Module):
+    def __init__(self, num_classes=4, do_dropout=False, embed_only=False):
+        super(_CNN6, self).__init__()
+
+        self.embed_only = embed_only
+        self.do_dropout = do_dropout
+        self.conv_block1 = ConvBlock5x5(in_channels=1, out_channels=64, stride=(1,1))
+        self.conv_block2 = ConvBlock5x5(in_channels=64, out_channels=128, stride=(1,1))
+        self.conv_block3 = ConvBlock5x5(in_channels=128, out_channels=256, stride=(1,1))
+        self.conv_block4 = ConvBlock5x5(in_channels=256, out_channels=512, stride=(1,1))
+        self.dropout = nn.Dropout(0.2)
+        self.linear = nn.Linear(512, num_classes, bias=True)
+
+    def forward(self, x):
+
+        x = self.conv_block1(x, pool_size=(2, 2), pool_type='avg')
+        if self.do_dropout:
+            x = self.dropout(x)
+        x = self.conv_block2(x, pool_size=(2, 2), pool_type='avg')
+        if self.do_dropout:
+            x = self.dropout(x)
+        x = self.conv_block3(x, pool_size=(2, 2), pool_type='avg')
+        if self.do_dropout:
+            x = self.dropout(x)
+        x = self.conv_block4(x, pool_size=(2, 2), pool_type='avg')
+        if self.do_dropout:
+            x = self.dropout(x)
+
+        x = torch.mean(x, dim=3) #mean over time dim
+        (x1, _) = torch.max(x, dim=2) #max over freq dim
+        x2 = torch.mean(x, dim=2) #mean over freq dim (after mean over time)
+        x = x1 + x2
+
+        if self.embed_only:
+            return x
+        return self.linear(x)
+
+class CNN6(nn.Module):
+    def __init__(self, num_classes=4, do_dropout=False, embed_only=False, from_scratch=False, path_to_weights="Cnn6_mAP=0.343.pth", device="cuda"):
+        super(CNN6, self).__init__()
+
+        self.cnn6 = _CNN6(num_classes=num_classes, do_dropout=do_dropout, embed_only=embed_only).to(device)
+        if not from_scratch:
+            weights = torch.load(path_to_weights, map_location=device)['model']
+            state_dict = {k: v for k, v in weights.items() if k in self.cnn6.state_dict().keys()}
+            self.cnn6.load_state_dict(state_dict, strict=False)
+
+    def forward(self, x):
+        return self.cnn6(x)
+
+class _CNN10(nn.Module):
+    def __init__(self, num_classes=4, do_dropout=False, embed_only=False):
+        super(_CNN10, self).__init__()
+
+        self.embed_only = embed_only
+        self.do_dropout = do_dropout
+        self.conv_block1 = ConvBlock(in_channels=1, out_channels=64)
+        self.conv_block2 = ConvBlock(in_channels=64, out_channels=128)
+        self.conv_block3 = ConvBlock(in_channels=128, out_channels=256)
+        self.conv_block4 = ConvBlock(in_channels=256, out_channels=512)
+        self.dropout = nn.Dropout(0.2)
+        self.linear = nn.Linear(512, num_classes, bias=True)
+
+    def forward(self, x):
+
+        x = self.conv_block1(x, pool_size=(2, 2), pool_type='avg')
+        if self.do_dropout:
+            x = self.dropout(x)
+        x = self.conv_block2(x, pool_size=(2, 2), pool_type='avg')
+        if self.do_dropout:
+            x = self.dropout(x)
+        x = self.conv_block3(x, pool_size=(2, 2), pool_type='avg')
+        if self.do_dropout:
+            x = self.dropout(x)
+        x = self.conv_block4(x, pool_size=(2, 2), pool_type='avg')
+        if self.do_dropout:
+            x = self.dropout(x)
+
+        x = torch.mean(x, dim=3) #mean over time dim
+        (x1, _) = torch.max(x, dim=2) #max over freq dim
+        x2 = torch.mean(x, dim=2) #mean over freq dim (after mean over time)
+        x = x1 + x2
+
+        if self.embed_only:
+            return x
+        return self.linear(x)
+
+class CNN10(nn.Module):
+    def __init__(self, num_classes=4, do_dropout=False, embed_only=False, from_scratch=False, path_to_weights="Cnn10_mAP=0.380.pth", device="cuda"):
+        super(CNN10, self).__init__()
+
+        self.cnn10 = _CNN10(num_classes=num_classes, do_dropout=do_dropout, embed_only=embed_only).to(device)
+        if not from_scratch:
+            weights = torch.load(path_to_weights, map_location=device)['model']
+            state_dict = {k: v for k, v in weights.items() if k in self.cnn10.state_dict().keys()}
+            self.cnn10.load_state_dict(state_dict, strict=False)
+
+    def forward(self, x):
+        return self.cnn10(x)
+
+class _CNN14(nn.Module):
+    def __init__(self, num_classes=4, do_dropout=False, embed_only=False):
+        super(_CNN14, self).__init__()
+
+        self.embed_only = embed_only
+        self.do_dropout = do_dropout
+        self.conv_block1 = ConvBlock(in_channels=1, out_channels=64)
+        self.conv_block2 = ConvBlock(in_channels=64, out_channels=128)
+        self.conv_block3 = ConvBlock(in_channels=128, out_channels=256)
+        self.conv_block4 = ConvBlock(in_channels=256, out_channels=512)
+        self.conv_block5 = ConvBlock(in_channels=512, out_channels=1024)
+        self.conv_block6 = ConvBlock(in_channels=1024, out_channels=2048)
+        self.dropout = nn.Dropout(0.2)
+        self.linear = nn.Linear(2048, num_classes, bias=True)
+
+    def forward(self, x):
+
+        x = self.conv_block1(x, pool_size=(2, 2), pool_type='avg')
+        if self.do_dropout:
+            x = self.dropout(x)
+        x = self.conv_block2(x, pool_size=(2, 2), pool_type='avg')
+        if self.do_dropout:
+            x = self.dropout(x)
+        x = self.conv_block3(x, pool_size=(2, 2), pool_type='avg')
+        if self.do_dropout:
+            x = self.dropout(x)
+        x = self.conv_block4(x, pool_size=(2, 2), pool_type='avg')
+        if self.do_dropout:
+            x = self.dropout(x)
+        x = self.conv_block5(x, pool_size=(2, 2), pool_type='avg')
+        if self.do_dropout:
+            x = self.dropout(x)
+        x = self.conv_block6(x, pool_size=(1, 1), pool_type='avg')
+        if self.do_dropout:
+            x = self.dropout(x)
+
+        x = torch.mean(x, dim=3) #mean over time dim
+        (x1, _) = torch.max(x, dim=2) #max over freq dim
+        x2 = torch.mean(x, dim=2) #mean over freq dim (after mean over time)
+        x = x1 + x2
+
+        if self.embed_only:
+            return x
+        return self.linear(x)
+
+class CNN14(nn.Module):
+    def __init__(self, num_classes=4, do_dropout=False, embed_only=False, from_scratch=False, path_to_weights="/users/local/Cnn14_mAP=0.431.pth", device="cuda"):
+        super(CNN14, self).__init__()
+
+        self.cnn14 = _CNN14(num_classes=num_classes, do_dropout=do_dropout, embed_only=embed_only)
+        if not from_scratch:
+            weights = torch.load(path_to_weights, map_location="cpu")['model']
+            state_dict = {k: v for k, v in weights.items() if k in self.cnn14.state_dict().keys()}
+            self.cnn14.load_state_dict(state_dict, strict=False)
+        self.cnn14 = self.cnn14.to(device)
+
+    def forward(self, x):
+        return self.cnn14(x)
+
+class Projector(nn.Module):
+    def __init__(self, name='cnn6', out_dim=128, apply_bn=False, device="cpu"):
+        super(Projector, self).__init__()
+        _, dim_in = model_dict[name]
+        self.linear1 = nn.Linear(dim_in, dim_in)
+        self.linear2 = nn.Linear(dim_in, out_dim)
+        self.bn = nn.BatchNorm1d(dim_in)
+        self.relu = nn.ReLU()
+        if apply_bn:
+            self.projector = nn.Sequential(self.linear1, self.bn, self.relu, self.linear2)
+        else:
+            self.projector = nn.Sequential(self.linear1, self.relu, self.linear2)
+        self.projector = self.projector.to(device)
+
+    def forward(self, x):
+        return self.projector(x)
+
+class LinearClassifier(nn.Module):
+    def __init__(self, name='cnn6', num_classes=4, device="cpu"):
+        super(LinearClassifier, self).__init__()
+        _, feat_dim = model_dict[name]
+        self.fc = nn.Linear(feat_dim, num_classes).to(device)
+
+    def forward(self, features):
+        return self.fc(features)
+
+def cnn6(**kwargs):
+    return CNN6(**kwargs)
+
+def cnn10(**kwargs):
+    return CNN10(**kwargs)
+
+def cnn14(**kwargs):
+    return CNN14(**kwargs)
+    
+model_dict = {
+    'cnn6' : [cnn6, 512],
+    'cnn10' : [cnn10, 512],
+    'cnn14' : [cnn14, 2048],
+}
+
+if __name__ == "__main__":
+    from torchinfo import summary
+    print("loading cnn14")
+    model = CNN10(num_classes=4, do_dropout=False, embed_only=True, from_scratch=True, path_to_weights="/users/local/i21moumm/panns/Cnn14_mAP=0.431.pth", device="cpu")
+    print(summary(model))
+    print("panns loaded")
+    quit()
